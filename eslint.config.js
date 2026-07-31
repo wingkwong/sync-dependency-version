0121203253b113a7a0345d2b1e8215e452049884
WK Wong <wingkwong.code@gmail.com>
2026-07-31 12:15:33 +0800

    refactor: replace by oxlint

--- eslint.config.js
+++
@@ -1,9 +0,0 @@
-const antfu = require('@antfu/eslint-config').default
-
-module.exports = antfu({
-  rules: {
-    'no-console': 'off',
-    'no-restricted-syntax': 'off',
-    'test/no-import-node-test': 'off',
-  },
-})
